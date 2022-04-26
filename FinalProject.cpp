bc23e550a2b9bfaba385d36e965c5dc57aebd20f
brown52230 <brown52230@mvctc>
2022-04-26 11:01:37 -0400

    added headers

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -3,8 +3,15 @@
 // April 25, 2022
 
 #include "SamPersonalLibrary.h"
-#include "item.h"
+
+#include "Item.h"
+#include "Video.h"
+#include "Music.h"
+#include "Game.h"
+#include "Book.h"
+
+int menu();
 
 int main(){
-
+    endProgram();
 }
